3c2918b3d30ec10ee85d9b6831ebee02240bfc09
eh3aneba <93880098+eh3aneba@users.noreply.github.com>
2026-09-13 20:02:39 +0330

    Register Phase 17.5-C authorization models

---
+++ apps/api/app/modules/external_document_sources/__init__.py
@@ -0,0 +1,9 @@
+from app.modules.external_document_sources.connection_authorization_models import (
+    ExternalDocumentSourceConnectionAuthorization,
+    ExternalDocumentSourceConnectionAuthorizationReceipt,
+)
+
+__all__ = [
+    "ExternalDocumentSourceConnectionAuthorization",
+    "ExternalDocumentSourceConnectionAuthorizationReceipt",
+]
